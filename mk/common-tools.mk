90e23157ee62c3f2f5adcc12b9d676288f0fcb5e
Masamichi Hosoda <trueroad@trueroad.jp>
2025-01-25 19:56:13 +0900

    Add auto-build model's makefile

--- mk/common-tools.mk
+++ mk/common-tools.mk
@@ -3,6 +3,7 @@ PDFTOCAIRO = pdftocairo
 
 SHOW_PDF_LINK = $(SCRIPT_DIR)/show_pdf_link.py
 CREATE_TICK_NOTE_RECT_LIST = $(SCRIPT_DIR)/create_tick_note_rect_list.py
+BUILD_MODEL_MAKEFILE = $(SCRIPT_DIR)/build_model_makefile.py
 
 MV = mv
 LN_S = ln -s
